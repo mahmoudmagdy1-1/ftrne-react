91906392c09b2421797aa7037aae6672c87bcd7b
Mahmoud Magdy <d4rkppl1@gmail.com>
2025-11-10 22:48:15 +0200

    initial commit - basic stuff added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { CiForkAndKnife } from "react-icons/ci";
+import { Link } from "react-router";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <nav className="h-20 flex justify-between items-center container mx-auto">
+        <Link
+          className="w-16 h-16 bg-amber-900 flex items-center justify-center rounded-2xl"
+          to={"/"}
+        >
+          <CiForkAndKnife size={32} />
+        </Link>
+        <div className="flex gap-4">
+          <Link to={"/orders/new"}>Quick Order</Link>
+          <Link to={"/orders"}>Past Orders</Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
